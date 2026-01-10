9f770e31f595629dd93300b04dff2fe76e53ac15
Dennis Sullivan <Dennis.Ocho@GMail.com>
2026-01-09 19:39:04 -0700

    Update letters-mexico.js

--- stories/letters-mexico.js
+++ stories/letters-mexico.js
@@ -12,8 +12,12 @@ function getLettersFromMexico() {
             url: "https://docs.google.com/document/d/e/2PACX-1vRhFK1aXrzhopNGNk7LTvEJq_-uEmPQKogtQCyU2MET831wJv4h1IQwJQT87mnXch2QfVPz3lY-55fz/preview"
         },
         {
-            date: "1991-12-26B",
+            date: "1991-12-26",
             url: "https://docs.google.com/document/d/e/2PACX-1vQGapTx_ySNAV4z-F44sGlGcUV4JF-OoekOXHDFj_bXVbHtorpS_0mdmsNp05gapV79XVebD_ur2Dzb/preview"
+        }
+        {
+            date: "1992-02-03",
+            url: "https://docs.google.com/document/d/e/2PACX-1vSfvf_vBRPqVVuBLNDo8osCSS_KyYH0uwkM0lcwClVEG56KYT6NRehWbRvL-oCzt5j83uerlh3DQ0Qe/preview"
         }
         // Add more letters here
     ].sort((a, b) => new Date(a.date) - new Date(b.date));
