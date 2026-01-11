028d3cd040b3dc8468e4c9bedff8daf6a53186b6
Dennis Sullivan <Dennis.Ocho@GMail.com>
2026-01-11 11:46:57 -0700

    Update letters-mexico.js

--- stories/letters-mexico.js
+++ stories/letters-mexico.js
@@ -16,143 +16,143 @@ function getLettersFromMexico() {
             url: "https://docs.google.com/document/d/1ZTgGFIPubHWnGke4b84TtKx9E8HOUzvNJXGiC1IaZ6U/preview"
         },
        {
-            title: "Letter1996-11-08-Bill",
+            title: "Fax 1996-11-08-Bill",
             url: "https://docs.google.com/document/d/18BYcuiGfzj6-FiVc2IYrtjYBMaqwHJ--btZ8gMHkn0s/preview"
         },
        {
-            title: "Letter1996-11-08-Francis",
+            title: "Fax 1996-11-08-Francis",
             url: "https://docs.google.com/document/d/194JcCxMgrNA_UfBtTBbbftkCLxcHkjmeka7887Ma3-Y/preview"
         },
        {
-            title: "Letter1996-11-15-Bill",
+            title: "Fax 1996-11-15-Bill",
             url: "https://docs.google.com/document/d/1liauDjCip57w0LO9qFJI2tSGzLUaQ8BUyEK8C1DaMlY/preview"
         },
        {
-            title: "Letter1996-11-16-Bill",
+            title: "Fax 1996-11-16-Bill",
             url: "https://docs.google.com/document/d/14jxazK7CSmvLsY0hZWhKdTp4P2qeO7_Kpj-hr0wl0hk/preview"
         },
        {
-            title: "Letter1996-11-21-Francis",
-            url: "https://docs.google.com/document/d/1oQjFvv8W11RI8tEprSVegV-pc5bS-Q4GMpBcqw4t9f0/edit?usp=sharing/preview"
+            title: "Fax 1996-11-21-Francis",
+            url: "https://docs.google.com/document/d/1oQjFvv8W11RI8tEprSVegV-pc5bS-Q4GMpBcqw4t9f0/preview"
         },
        {
-            title: "Letter1996-11-23-Bill-Martin",
+            title: "FAX 1996-11-23-Bill-Martin",
             url: "https://docs.google.com/document/d/1n4ik9JuOCeSao1Nbz44LQFv1zXLWAsSCvCFHpgEVPjk/preview"
         },
        {
-            title: "Letter1996-11-29",
+            title: "FAX 1996-11-29",
             url: "https://docs.google.com/document/d/1nKP9kMbKOiVWXQdQBfQAJjgl7ehV7svpR7bGJJziT2c/preview"
         },
        {
-            title: "Letter1996-12-02",
+            title: "FAX 1996-12-02",
             url: "https://docs.google.com/document/d/1CCCKvnL250PKA0uem1EClnQYSy8e4GJOx9YaPK7KoWo/preview"
         },
        {
-            title:Letter1996-12-02-ps"",
+            title:"FAX 1996-12-02-ps",
             url: "https://docs.google.com/document/d/1kYpxBI8bc1umuopR8VMqVFTlAHLiLaRTVl5yFghu-GA/preview"
         },
        {
-            title: "Letter1996-12-10-Bill",
+            title: "FAX 1996-12-10-Bill",
             url: "https://docs.google.com/document/d/1GFk25fe48pKUvFAR8-kenOysufOx-2O5pWoc9XkNq80/preview"
         },
        {
-            title: "Letter1996-12-13",
+            title: "FAX 1996-12-13",
             url: "https://docs.google.com/document/d/1pB2xvR32ji9Hs6PGasL9LIy77HFqf3DkMvWXRpShi7k/preview"
         },
        {
-            title: "Letter1996-12-16-Peter",
+            title: "FAX 1996-12-16-Peter",
             url: "https://docs.google.com/document/d/1VbtMIhv-XT4EFWabDGVhT6a9IE0F6aiJK716VmRDnCs/preview"
         },
        {
-            title: "Letter1996-12-17-Peter",
+            title: "FAX 1996-12-17-Peter",
             url: "https://docs.google.com/document/d/17emULX1nyACA6nuxMtG1AUuQQvhBA6Wl490OebvoVIY/preview"
         },
        {
-            title: "Letter1996-12-18",
+            title: "FAX 1996-12-18",
             url: "https://docs.google.com/document/d/1Hm5ZJfZ02SATVq0nK78fFlDC8vDuB5GDgMjTKyKzF_U/preview"
         },
        {
-            title: "Letter1996-12-22-Joe",
+            title: "FAX 1996-12-22-Joe",
             url: "https://docs.google.com/document/d/1oD7m-nXCILjRqNPN_vTD7RFco3V7vD9a7iX6BpCUNGo/preview"
         },
        {
-            title: "Letter1996-12-25-Florence",
+            title: "FAX 1996-12-25-Florence",
             url: "https://docs.google.com/document/d/1SFGpSAgYolsb3MO_NrDKOubsrrFuux5H1vTVS31UMUE/preview"
         },
        {
-            title: "Letter1996-12-29-Francis",
+            title: "FAX 1996-12-29-Francis",
             url: "https://docs.google.com/document/d/1-Y9DvRv-pWZuVsgigXGLUhhHNKATGwrk6M4C7GHAqJg/preview"
         },
        {
-            title: "Letter1997-01-04",
+            title: "FAX 1997-01-04",
             url: "https://docs.google.com/document/d/1UX--vH-M42MAYah53QjmDlw26XAYgV7s1lPvFOu4coE/preview"
         },
        {
-            title: "Letter1997-01-06-Sheila",
+            title: "FAX 1997-01-06-Sheila",
             url: "https://docs.google.com/document/d/1w4IcWE4ezqZRQnDabSyubu6PLVdFeltsbowQQWfzaQY/preview"
         },
        {
-            title: "Letter1997-01-07-Kathy",
+            title: "FAX 1997-01-07-Kathy",
             url: "https://docs.google.com/document/d/1m2R4yyd1I8ab6mtaN3NVVQItzxzQt-JOeihOnCu9g2E/preview"
         },
        {
-            title: "Letter1997-01-12",
+            title: "FAX 1997-01-12",
             url: "https://docs.google.com/document/d/1TXzX-nAvPQLSaQUjVkTEc7-4_DH4hO3abRT05IDukzc/preview"
         },
        {
-            title: "Letter1997-01-14-Joe",
+            title: "FAX 1997-01-14-Joe",
             url: "https://docs.google.com/document/d/1Lx1AO0ZQsxAZb395_J27sBEZjLRhDe9inXjVH_1O9mE/preview"
         },
        {
-            title: "Letter1997-01-22-Tom",
+            title: "FAX 1997-01-22-Tom",
             url: "https://docs.google.com/document/d/163PR8Zy0pwTgESk1RGjJ5Rmc9UQ633L92m16n2nuKAg/preview"
         },
        {
-            title: "Letter1997-01-28-Bill",
+            title: "FAX 1997-01-28-Bill",
             url: "https://docs.google.com/document/d/1SXqXMvK4TDZcV7OCU4o6VDCDv3AM6l62qes1sF55EhA/preview"
         },
        {
-            title: "Letter1997-01-28-Steven",
+            title: "FAX 1997-01-28-Steven",
             url: "https://docs.google.com/document/d/1jwwFP2xLU00FmkKWAMcesdSXfI6Vilwghn0XGcHMUOs/preview"
         },
        {
-            title: "Letter1997-01-30-Francis",
+            title: "FAX 1997-01-30-Francis",
             url: "https://docs.google.com/document/d/1GjgF537cRbPvzXOYz7vuNivxN3HXB6WnAmu4lJsIKwU/preview"
         },
        {
-            title: "Letter1997-01-30-Linda-Teresa",
+            title: "FAX 1997-01-30-Linda-Teresa",
             url: "https://docs.google.com/document/d/1FkUCMCBBH-En_XQ2cLrIEvhyFMxcDMnqd7yQygL5abg/preview"
         },
        {
-            title: "Letter1997-02-04-Maureen and Derk",
+            title: "FAX 1997-02-04-Maureen and Derk",
             url: "https://docs.google.com/document/d/1D9qgre4U6UaCpipEN83W3zpTcJ7yHupYIKHkb3iaQCk/preview"
         },
        {
-            title: "Letter1997-02-10-Sheila-John",
+            title: "FAX 1997-02-10-Sheila-John",
             url: "https://docs.google.com/document/d/1uJg6ENu3FR9PjlEq-7pZ0zE4P0_I6d72hRvk7yrCvX8/preview"
         },
        {
-            title: "Letter1997-02-13",
+            title: "FAX 1997-02-13",
             url: "https://docs.google.com/document/d/16LFipcedzEUkmtaXWh8AphwYNBmuOiGqmagCUYKMor0/preview"
         },
        {
-            title: "Letter1997-02-14-Francis",
+            title: "FAX 1997-02-14-Francis",
             url: "https://docs.google.com/document/d/1-1c9r71T-eSidCkTt8mskuPfL7VbW28yt4FNTRM95dE/preview"
         },
        {
-            title: "Letter1997-02-15-Mary",
+            title: "FAX 1997-02-15-Mary",
             url: "https://docs.google.com/document/d/1Xolqwt9lGBkgEq1L4HDMUzLQB7wvNS_BjlY331XV-9k/preview"
         },
        {
-            title: "Letter1997-02-21-Dennis",
+            title: "FAX 1997-02-21-Dennis",
             url: "https://docs.google.com/document/d/1HOJaCtkaIo9JFibO9--pBtSLjMVhSk-VZIVbt4Za7Mk/preview"
         },
        {
-            title: "Letter1997-02-28-Tessa",
-            url: "https://docs.google.com/document/d/1y8narKWkqn6UdUgG9O-Cd-2oCkie321CIvq91DtSl_8/edit?usp=sharing/preview"
+            title: "FAX 1997-02-28-Tessa",
+            url: "https://docs.google.com/document/d/1y8narKWkqn6UdUgG9O-Cd-2oCkie321CIvq91DtSl_8/preview"
         },
         {
-            title: "Letter1997-02-28-StephenPolk",
+            title: "FAX 1997-02-28-StephenPolk",
             url: "https://docs.google.com/document/d/1ncvBzlzuBgwY4ucSN9_Okke6OTeEvC1_LQZPFt2w5uI/preview"
         }
         // Add more letters here, in order
