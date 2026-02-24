c09daecfa90b3d07cd55218c3171329f5c97c917
Dennis Sullivan <Dennis.Ocho@GMail.com>
2026-02-24 15:45:58 -0700

    Update dominoRenderer.js

--- puzzles/pips/ui/dominoRenderer.js
+++ puzzles/pips/ui/dominoRenderer.js
@@ -68,7 +68,7 @@ export function renderDomino(domino, wrapper) {
   // ------------------------------------------------------------
   // Build DOM structure
   // ------------------------------------------------------------
-  const inner = document.createElement("div");
+  inner = document.createElement("div");
   inner.className = "domino";
   // ------------------------------------------------------------
   // Orientation (board only)
